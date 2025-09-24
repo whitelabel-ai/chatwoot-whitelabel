922044c6260c8682faaaaaa27a67d19e0d54e856
propiter <pro.pedro0413@gmail.com>
2025-09-23 23:31:22 -0500

    fix _HUB_URL to whitelabel

--- deployment/setup_20.04.sh
+++ deployment/setup_20.04.sh
@@ -21,7 +21,7 @@ LONGOPTS=console,debug,help,install,Install:,logs:,restart,ssl,upgrade,Upgrade:,
 OPTIONS=cdhiI:l:rsuU:wvWK
 CWCTL_VERSION="3.4.3"
 pg_pass=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 15 ; echo '')
-CHATWOOT_HUB_URL="https://hub.2.chatwoot.com/events"
+CHATWOOT_HUB_URL="https://hub.2.whitelabel.lat/events"
 
 # if user does not specify an option
 if [ "$#" -eq 0 ]; then
@@ -1145,7 +1145,7 @@ function report_event() {
   local event_name="$1"
   local event_data="$2"
 
-  CHATWOOT_HUB_URL="https://hub.2.chatwoot.com/events"
+  CHATWOOT_HUB_URL="https://hub.2.whitelabel.lat/events"
 
   # get installation identifier
   local installation_identifier=$(get_installation_identifier)
